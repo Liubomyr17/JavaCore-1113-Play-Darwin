72b73799ddb3b001bcadd4db33abc9b05eaa7d84
Code First! <41821541+Liubomyr17@users.noreply.github.com>
2020-02-29 11:00:34 +0200

    Add files via upload

---
+++ JavaCore 1113 Play Darwin/src/com/company/Main.java
@@ -0,0 +1,47 @@
+package com.company;
+
+/*
+
+1113 Play Darwin
+Change four classes: Fish (Fish), Animal (Animal), Ape (Monkey), Human (Man).
+Inherit an animal from fish, a monkey from an animal, and a person from a monkey.
+Requirements:
+1. In the class Solution there must be a public class Fish (Fish).
+2. The Solution class must have a public Animal class.
+3. In the class Solution there must be a public class Ape (Monkey).
+4. In the class Solution there must be a public class Human.
+5. The class Animal must be inherited from the class Fish.
+6. The Ape class must be inherited from the Animal class.
+7. The Human class must be inherited from the Ape class.
+
+ */
+
+import java.io.*;
+import java.lang.reflect.Array;
+import java.net.URISyntaxException;
+import java.nio.file.FileSystemException;
+import java.nio.file.attribute.AclFileAttributeView;
+import java.rmi.RemoteException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+    }
+    public class Fish{
+    }
+    public class Animal extends Fish {
+    }
+    public class Ape extends Animal {
+    }
+    public class Human extends Ape {
+    }
+}
+
+
+
+
+
+
+
